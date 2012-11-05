4ab27073eb5d2069fc3bd0eddbac01c8c7959569
JP Toto <james.p.toto@gmail.com>
2012-11-04 19:48:48 -0500

    Added IOStreamToByteArray

---
+++ IOStreamtoByteArray.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace ConsoleApplication1
+{
+    public static class IOStreamtoByteArray
+    {
+        /// <summary>
+        /// Helper function to convert System.IO.Stream data to a byte[] array
+        /// </summary>
+        /// <param name="input">Any System.IO.Stream input</param>
+        /// <returns></returns>
+        public static byte[] ReadFully(Stream input)
+        {
+            var buffer = new byte[16 * 1024];
+            using (var ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
+        }
+    }
+}
