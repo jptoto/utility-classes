a0f1a194b835c45482f44265fb066c73398a13c2
JP Toto <james.p.toto@gmail.com>
2012-11-27 07:22:11 -0500

    Added mysqlconnect and fileparser classes

---
+++ FileParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ConnectCsharpToMysql
+{
+    public class FileParser
+    {
+        public static List<string> parseCSV(string path)
+        {
+            List<string> parsedData = new List<string>();
+
+            try
+            {
+                using (StreamReader readFile = new StreamReader(path))
+                {
+                    string line;
+                    //string[] row;
+
+                    while ((line = readFile.ReadLine()) != null)
+                    {
+                        //row = line.Split(',');
+                        parsedData.Add(line);
+                    }
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            return parsedData;
+        }
+    }
+}
